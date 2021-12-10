071aaefdf371ebddb62c350af4ee7dfa86cc4165
rregalado17 <rregalado17@gmail.com>
2021-12-10 00:15:32 -0500

    navbar development

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
-import { Layout, Typography, Space } from 'antd'
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components'
+import './App.css'
 
 const App = () => {
     return (
         <div className="app">
             <div className="navbar">
-
+                <Navbar />
             </div>
             <div className="navbar">
-
+                <Layout>
+                    <div className='routes'>
+                        <Switch>
+                            <Route exact path='/'>
+                                <Homepage />
+                            </Route>
+                            <Route exact path='/exchanges'>
+                                <Exchanges />
+                            </Route>
+                            <Route exact path='/cryptocurrencies'>
+                                <Cryptocurrencies />
+                            </Route>
+                            <Route exact path='/crypto/:coinId'>
+                                <CryptoDetails />
+                            </Route>
+                            <Route exact path='/news'>
+                                <News />
+                            </Route>
+                        </Switch>
+                    </div>
+                </Layout>
             </div>
             <div className="footer">
 
