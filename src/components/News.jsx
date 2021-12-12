5a7ed88994ca307a971c1ecfdccf0e1485144f6b
rregalado17 <rregalado17@gmail.com>
2021-12-11 21:17:28 -0500

    draft news api

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 
-const News = () => {
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ( {simplified}) => {
+
+    const { data: cryptoNews } = useGetCryptoNewsQuery( {newsCategory: 'Cryptocurrency', count: simplified ? 10:100});
+
+    console.log(cryptoNews)
+
     return (
         <div>
             News
