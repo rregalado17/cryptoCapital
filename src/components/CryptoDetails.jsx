dd72db889d4db5adfb5b9e99461b7b608ccf8900
rregalado17 <rregalado17@gmail.com>
2022-01-06 00:40:50 -0500

    draft bitcoin statistics and details

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -58,6 +58,66 @@ export const CryptoDetails = () => {
                         {time.map((date) => <Option key={date}>{date}</Option>)}
 
                 </Select>
+                <Col className='='stats-container>
+                    <Col className='coin-value-statistics'>
+                        <Col className='coin-value-statistics-heading'>
+                            <Title level={3} className='coin-details-heading'> 
+                                {cryptoDetails.name} Value Statistics
+                            </Title>
+                            <p>
+                                An overview showing the stats of {cryptoDetails.name}
+                            </p>
+                        </Col>
+                        {stats.map(({icon, title, value}) =>  (
+                            <Col className='coin-stats'>
+                                <Col className='coin-stats-name'>
+                                    <Text>{icon}</Text>
+                                    <Text>{title}</Text>
+                                </Col>
+                                <Text className='stats'>
+                                    {value}
+                                </Text>
+                            </Col>
+                        ))}
+                    </Col>
+                    <Col className='other-stats-info'>
+                        <Col className='coin-value-statistics-heading'>
+                            <Title level={3} className='coin-details-heading'> 
+                                Other Statistics
+                            </Title>
+                            <p>
+                                An overview showing all cryptocurrencies.
+                            </p>
+                        </Col>
+                        {genericStats.map(({icon, title, value}) =>  (
+                            <Col className='coin-stats'>
+                                <Col className='coin-stats-name'>
+                                    <Text>{icon}</Text>
+                                    <Text>{title}</Text>
+                                </Col>
+                                <Text className='stats'>
+                                    {value}
+                                </Text>
+                            </Col>
+                        ))}
+                    </Col>
+                </Col>
+                <Col className='coin-desc-link'>
+                    <Row className='coin-desc'>
+                        <Title level={3} className='coin-details-heading'>
+                            What is {cryptoDetails.name}?
+                            {HTMLReactParser(cryptoDetails.description)}
+                        </Title>
+                    </Row>
+                    <Col className='coin-links'>
+                        <Title level={3} className='coin-details-heading'>{cryptoDetails.name} Links</Title>
+                        {cryptoDetails.links.map((link) => 
+                        <Row className='coin-link' key={link.name}>
+                            <Title level={5} className='link-name'>{link.type}</Title>
+                            <a href={link.url} target='_blank' rel='noreferre'>{link.name}</a>
+                        </Row>)}
+                    </Col>
+                </Col>
             </Col>
 
         </div>
