7a2cfa716ffb7999eacebca9984ef650717c705a
rregalado17 <rregalado17@gmail.com>
2022-01-05 23:07:48 -0500

    debug crypto details component

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -2,11 +2,11 @@ import React, { useState } from 'react'
 import HTMLReactParser from 'html-react-parser';
 import { useParams } from 'react-router-dom'
 import millify from 'millify';
-import {Column, Row, Typography, Select } from 'antd';
+import {Col, Row, Typography, Select } from 'antd';
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
 
-const { Tile, Text } = Typography;
+const { Title, Text } = Typography;
 const { Option } = Select;
 
 export const CryptoDetails = () => {
@@ -19,24 +19,47 @@ export const CryptoDetails = () => {
     const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
     const stats = [
-        { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
-        { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
-        { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
-        { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
-        { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+      { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+      { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+      { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+      { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+      { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+    ];
+  
+    const genericStats = [
+      { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+      { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+      { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+      { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+      { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
     ];
 
-    const genericStats = [
-        { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
-        { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
-        { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
-        { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
-        { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
-    ];
+
 
     return (
         <div>
-            Crypto Details {coinId}
+            <Col className='coin-detail-container'>
+                <Col className='coin-heading-container'>
+                    <Title level={2} className='coin-name'>
+                        {cryptoDetails.name} ({ cryptoDetails.slug}) Price
+
+                    </Title>
+                    <p>
+                        {cryptoDetails.name} Live price in US dollars.
+                        View value statistics, market cap and supple.
+                    </p>
+                </Col>
+                <Select 
+                    defaultValue='7d' 
+                    className='select-timeperiod' 
+                    placeholder='Select Time Period'
+                    onChange={(value ) => setTimePeriod(value)}
+                    >
+                        {time.map((date) => <Option key={date}>{date}</Option>)}
+
+                </Select>
+            </Col>
+
         </div>
     )
 }
