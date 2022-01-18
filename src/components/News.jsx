2317379797fc8b489ceb5082e8b0df56ec497416
rregalado17 <rregalado17@gmail.com>
2022-01-17 21:53:51 -0500

    troubleshoot bugs

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -17,23 +17,23 @@ const News = ( {simplified}) => {
     if(!cryptoNews?.value) return 'Loading...';
 
     return (
-        <><Row gutter={[24, 24]}>
-            {!simplified && (
-                <Col span={24}>
-                     <Select
-                        showSearch
-                        className='select-news'
-                        placeholder='Select a Crypto'
-                        optionFilterProp='children'
-                        onChange={(value) => setNewsCategory(value)}
-                        filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                        >
-                        <Option value="Cryptocurrency">Cryptocurrency</Option>
-                        {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name} </Option>)}
-                     </Select>
-                </Col>
-            )}
-
+        <>
+            <Row gutter={[24, 24]}>
+                {!simplified && (
+                    <Col span={24}>
+                        <Select
+                            showSearch
+                            className='select-news'
+                            placeholder='Select a Crypto'
+                            optionFilterProp='children'
+                            onChange={(value) => setNewsCategory(value)}
+                            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+                            >
+                            <Option value="Cryptocurrency">Cryptocurrency</Option>
+                            {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name} </Option>)}
+                        </Select>
+                    </Col>
+                )}
 
             {cryptoNews.value.map((news, i) => (
                 <Col xs={12} sm={12} lg={8} key={i}> 
